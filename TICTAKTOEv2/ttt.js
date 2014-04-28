ebaf1767e396c23256fa63e9013b8228b77c1eea
William Cosmo Taormina <williamtaormina@gmail.com>
2014-04-27 22:52:06 -0700

    Updates

--- TICTAKTOEv2/ttt.js
+++ TICTAKTOEv2/ttt.js
@@ -1,4 +1,5 @@
 function Ticktackctrl($scope){
+
 //this is the board, represented as an array.
 
 	$scope.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
@@ -22,6 +23,7 @@ function Ticktackctrl($scope){
 
 		$scope.checkWin();
 	};
+
 	$scope.resetboard = function (){$scope.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];};
 
 	$scope.checkWin = function (){
@@ -29,20 +31,20 @@ function Ticktackctrl($scope){
 		// horizontal wins
 		if($scope.board[0]==$scope.board[1] && $scope.board[1] == $scope.board[2] && $scope.board[0] != " ")
 			{
-			winner = $scope.board[0];
-			console.log("The winner is " + winner);
+			$scope.winner = $scope.board[0];
+			console.log("The winner is " + $scope.winner);
 			alert("You Won");
 			$scope.resetboard();
 			}
 		else if ($scope.board[3]==$scope.board[4] && $scope.board[4] == $scope.board[5] && $scope.board[3] != " ")
 			{
-			winner = $scope.board[3];
+			$scope.winner = $scope.board[3];
 			console.log(winner);
 			$scope.resetboard();
 			}
 		else if ($scope.board[6]==$scope.board[7] && $scope.board[7] == $scope.board[8] && $scope.board[6] != " ")
 			{
-			winner = $scope.board[3];
+			$scope.winner = $scope.board[3];
 			console.log("winner");
 			$scope.resetboard();
 			}
@@ -50,19 +52,19 @@ function Ticktackctrl($scope){
 		// vertical wins
 		else if($scope.board[0]==$scope.board[3] && $scope.board[3] == $scope.board[6] && $scope.board[0] != " ")
 			{
-			winner = $scope.board[0];
+			$scope.winner = $scope.board[0];
 			console.log("winner");
 			$scope.resetboard();
 			}
 		else if ($scope.board[1]==$scope.board[4] && $scope.board[4] == $scope.board[7] && $scope.board[1] != " ")
 			{
-			winner = $scope.board[3];
+			$scope.winner = $scope.board[3];
 			console.log("winner");
 			$scope.resetboard();
 			}
 		else if ($scope.board[2]==$scope.board[5] && $scope.board[5] == $scope.board[8] && $scope.board[2] != " ")
 			{
-			winner = $scope.board[3];
+			$scope.winner = $scope.board[3];
 			console.log("winner");
 			$scope.resetboard();
 			}
@@ -71,7 +73,7 @@ function Ticktackctrl($scope){
 
 		else if ($scope.board[0]==$scope.board[4] && $scope.board[4] == $scope.board[8] && $scope.board[0] != " ")
 			{
-			winner = $scope.board[3];
+			$scope.winner = $scope.board[3];
 			console.log("winner");
 			$scope.resetboard();
 			}
@@ -82,7 +84,63 @@ function Ticktackctrl($scope){
 			$scope.resetboard();
 			}
 
-	
+	$scope.checkTie = function (){
+
+		if($scope.board[0]!=$scope.board[1] && $scope.board[0] != " ")
+			{
+			$scope.winner = $scope.board[0];
+			console.log("The winner is " + $scope.winner);
+			alert("You Won");
+			$scope.resetboard();
+			}
+		else if ($scope.board[3]==$scope.board[4] && $scope.board[4] == $scope.board[5] && $scope.board[3] != " ")
+			{
+			$scope.winner = $scope.board[3];
+			console.log(winner);
+			$scope.resetboard();
+			}
+		else if ($scope.board[6]==$scope.board[7] && $scope.board[7] == $scope.board[8] && $scope.board[6] != " ")
+			{
+			$scope.winner = $scope.board[3];
+			console.log("winner");
+			$scope.resetboard();
+			}
+
+		// vertical wins
+		else if($scope.board[0]==$scope.board[3] && $scope.board[3] == $scope.board[6] && $scope.board[0] != " ")
+			{
+			$scope.winner = $scope.board[0];
+			console.log("winner");
+			$scope.resetboard();
+			}
+		else if ($scope.board[1]==$scope.board[4] && $scope.board[4] == $scope.board[7] && $scope.board[1] != " ")
+			{
+			$scope.winner = $scope.board[3];
+			console.log("winner");
+			$scope.resetboard();
+			}
+		else if ($scope.board[2]==$scope.board[5] && $scope.board[5] == $scope.board[8] && $scope.board[2] != " ")
+			{
+			$scope.winner = $scope.board[3];
+			console.log("winner");
+			$scope.resetboard();
+			}
+
+			// diagonal wins
+
+		else if ($scope.board[0]==$scope.board[4] && $scope.board[4] == $scope.board[8] && $scope.board[0] != " ")
+			{
+			$scope.winner = $scope.board[3];
+			console.log("winner");
+			$scope.resetboard();
+			}
+		else if ($scope.board[2]==$scope.board[4] && $scope.board[4] == $scope.board[6] && $scope.board[2] != " ")
+			{
+			$scope.winner = $scope.board[3];
+			console.log("winner");
+			$scope.resetboard();
+			}
+	};
 
 	};
 
